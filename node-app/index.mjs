ce77f1bd1154775c52f6332e53953caf6ac3e0f6
Ilya <illyaetoilysha@gmail.com>
2024-01-24 12:09:40 +0200

    commit all files

---
+++ node-app/index.mjs
@@ -0,0 +1,8 @@
+import fs from 'fs';
+
+fs.appendFile('message.txt', 'data to append', (err) => {
+    if (err) throw err;
+    console.log('The "data to append" was appended to file!');
+});
+
+setTimeout(() => console.log('you have 30 seconds!'), 30000);
---
+++ python-app/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('Hello BIG World!\n')
+
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+
+print(calendar.month(year, month))
+
+print('\nGoodbye World!')
